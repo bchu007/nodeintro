7611bdf13d392b55ec84ae15e3f504aa3874779d
Brian Chu <brianchu14@gmail.com>
2019-08-05 12:22:15 -0700

    initial files

---
+++ calc.js
@@ -0,0 +1,19 @@
+function doMath( num1, operator, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    var calculateMap = {
+        '+': (num1, num2)=>num1+num2,
+        '*': (num1, num2)=>num1*num2,
+        '/': (num1, num2)=>num1/num2,
+        '-': (num1, num2)=>num1-num2,
+    }
+    calculateMap['x'] = calculateMap['X'] = calculateMap['*'];
+    var result = calculateMap[operator](num1, num2);
+    return result;
+}
+
+var num1 = process.argv[2];
+var num2 = process.argv[4];
+var operator = process.agrv[3];
+
+console.log( doMath(num1, operator, num2) );
